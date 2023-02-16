870cf3fdf1b783657994e00c71bf24b271baa41d
GabrielaMartins19 <gabi.martins1909@gmail.com>
2023-02-16 18:16:50 +0000

    Fixtures and mocks

---
+++ life_expectancy/tests/fixtures/mock.py
@@ -0,0 +1,30 @@
+def raw_data() -> dict:
+    """
+    Function to mock row data.
+    :return: Dict with data
+    """
+    data = {
+        'unit': ['YR', 'YR', 'YR'],
+        'sex': ['F', 'F', 'F'],
+        'age': ['Y1', 'Y1', 'Y1'],
+        'geo\time': ['AM', 'AL', 'PT'],
+        '2021': ['79.4', '80.4', '80.2'],
+        '2020': ['79.1', '79.2', '78.5'],
+        '2019': ['82.9', '83.5', '83.3']
+    }
+    return data
+
+def expect_data() -> dict:
+    """
+    Function to mock expected data.
+    :return: Dict with data.
+    """
+    data = {
+        'unit': ['YR', 'YR', 'YR'],
+        'sex': ['F', 'F', 'F'],
+        'age': ['Y1', 'Y1', 'Y1'],
+        'region': ['PT', 'PT', 'PT'],
+        'year': [2021, 2020, 2019],
+        'value': [80.2, 78.5, 83.3]
+    }
+    return data
--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,16 +1,36 @@
 """Tests for the cleaning module"""
 import pandas as pd
+import pytest
+from unittest.mock import patch, Mock
 
-from life_expectancy.cleaning import main
+from life_expectancy.cleaning import main, load_data, clean_data
+from life_expectancy.tests.fixtures.mock import raw_data, expect_data
+
 from . import OUTPUT_DIR
 
+@pytest.fixture
+def fixture_raw_data():
+    return pd.DataFrame(raw_data())
 
-def test_clean_data(pt_life_expectancy_expected):
-    """Run the `clean_data` function and compare the output to the expected output"""
-    main()
-    pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
-    )
+@pytest.fixture
+def fixture_expected_data():
+    return pd.DataFrame(expect_data())
+
+@patch('pandas.read_csv')
+def test_load_data(read_table_mock: Mock, fixture_raw_data):
+    read_table_mock.return_value = fixture_raw_data
+    results = load_data()
+    read_table_mock.assert_called_once()
+
+    pd.testing.assert_frame_equal(results, fixture_raw_data)
+
+
+def test_clean_data(fixture_raw_data, fixture_expected_data):
+    pt_life_expectancy_data = clean_data(
+        fixture_raw_data,
+        region='PT'
+    ).reset_index(drop=True)
+
     pd.testing.assert_frame_equal(
-        pt_life_expectancy_actual, pt_life_expectancy_expected
+        pt_life_expectancy_data, fixture_expected_data
     )
